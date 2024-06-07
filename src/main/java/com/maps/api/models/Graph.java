f8f9c79b7f33ec680683f0c04ecc25f9540cf000
unknown <ALEXANDRE@LAPTOP-2945L9RP>
2024-06-07 14:06:35 +0200

    .\Graph.java

--- src/main/java/com/maps/api/models/Graph.java
+++ src/main/java/com/maps/api/models/Graph.java
@@ -1,47 +1,54 @@
 package com.maps.api.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
     private String name;
     private int height;
     private int width;
-    private ArrayList<Node> Nodes;
+    private List<Node> nodes;
 
-    public Graph {
+    public Graph(String name, int height, int width) {
         this.name = name;
         this.height = height;
         this.width = width;
         this.nodes = new ArrayList<>();
     }
 
-    public void addNode(Node) {
-        nodes.add(Node);
+    public void init() {
+        this.nodes = new ArrayList<>();
     }
 
-    public void remouveNode(str) {
-        Node nodeToRemove = findNodeByName(str);
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+
+    public void removeNode(String nodeName) {
+        Node nodeToRemove = findNodeByName(nodeName);
         if (nodeToRemove != null) {
-            for (Node : nodes) {
-                Node.removeNode(nodeToRemove);
+            for (Node node : nodes) {
+                node.removeNode(nodeToRemove);
             }
             nodes.remove(nodeToRemove);
         }
     }
 
-    public void addLiaison(str1, str2) {
-        Node node1 = findNodeByName(str1);
-        Node node2 = findNodeByName(str2);
+    public void addLiaison(String nodeName1, String nodeName2) {
+        Node node1 = findNodeByName(nodeName1);
+        Node node2 = findNodeByName(nodeName2);
         if (node1 != null && node2 != null) {
             node1.addNode(node2);
             node2.addNode(node1);
         }
     }
 
-    public remouveLiaison(str1, str2) {
-        Node node1 = findNodeByName(str1);
-        Node node2 = findNodeByName(str2);
+    public void removeLiaison(String nodeName1, String nodeName2) {
+        Node node1 = findNodeByName(nodeName1);
+        Node node2 = findNodeByName(nodeName2);
         if (node1 != null && node2 != null) {
             node1.removeNode(node2);
-            node2.removeNode(node1);  // Si la liaison est bidirectionnelle
+            node2.removeNode(node1);
         }
     }
 
@@ -66,8 +73,7 @@ public class Graph {
         return width;
     }
 
-    public ArrayList<Node> getNodes() {
+    public List<Node> getNodes() {
         return nodes;
     }
 }
-
