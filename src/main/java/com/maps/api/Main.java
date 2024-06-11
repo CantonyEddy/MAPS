d3e9d73092e1fe87520ec61ea872a6a3f70f53a2
alenzo <alenzo.amico@gmail.com>
2024-06-11 15:14:48 +0200

    test

--- src/main/java/com/maps/api/Main.java
+++ src/main/java/com/maps/api/Main.java
@@ -5,7 +5,7 @@ import com.maps.api.console.Display.*;
 
 public class Main {
     public static void main(String[] args) {
-        Display display = new Display("truc", 50, 50, "♥");
+        Display display = new Display("truc", 50, 50, "-");
         System.out.println(display.getName());
         display.printScreen();
         display.addPoint(4, 5, 'p');
